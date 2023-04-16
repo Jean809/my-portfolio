e691c7d3f70b4120a417fbec0345bf0ad67acc33
Jean809 <jean_carlosrosario709@outlook.com>
2023-04-16 11:37:22 -0400

    data file created and styled cards w/ grid

--- src/App.js
+++ src/App.js
@@ -3,15 +3,41 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Card from "./components/Card";
 import Skills from "./components/Skills";
+import data from "./components/data";
 
 function App() {
+  const card = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        title={item.title}
+        badge1={item.badge1}
+        badge2={item.badge2}
+        badge3={item.badge3}
+        description={item.description}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <Header />
       <Home />
       <About />
       <Skills />
-      <Card />
+      <div className="card-container">{card}</div>
+
+      {/*
+      <Card
+
+      
+        title="Guess-My-Number"
+        badge1="images/badges/JavaScript.png"
+        badge2="images/badges/HTML.png"
+        badge3="images/badges/CSS.png"
+        description="A game with a scoring system that measures the highest score obtained
+        as you are guessing the correct numbers."
+  />*/}
     </div>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    id: 1,
+    title: "Guess-My-Number",
+    badge1: "images/badges/JavaScript.png",
+    badge2: "images/badges/HTML.png",
+    badge3: "images/badges/CSS.png",
+    badge4: "",
+    description:
+      "A game with a scoring system that measures the highest score obtained as you are guessing the correct numbers.",
+  },
+  {
+    id: 2,
+    title: "Guess-My-Number",
+    badge1: "images/badges/JavaScript.png",
+    badge2: "images/badges/HTML.png",
+    badge3: "images/badges/CSS.png",
+    badge4: "",
+    description:
+      "A game with a scoring system that measures the highest score obtained as you are guessing the correct numbers.",
+  },
+  {
+    id: 2,
+    title: "Guess-My-Number",
+    badge1: "images/badges/JavaScript.png",
+    badge2: "images/badges/HTML.png",
+    badge3: "images/badges/CSS.png",
+    badge4: "",
+    description:
+      "A game with a scoring system that measures the highest score obtained as you are guessing the correct numbers.",
+  },
+  {
+    id: 2,
+    title: "Guess-My-Number",
+    badge1: "images/badges/JavaScript.png",
+    badge2: "images/badges/HTML.png",
+    badge3: "images/badges/CSS.png",
+    badge4: "",
+    description:
+      "A game with a scoring system that measures the highest score obtained as you are guessing the correct numbers.",
+  },
+  {
+    id: 2,
+    title: "Guess-My-Number",
+    badge1: "images/badges/JavaScript.png",
+    badge2: "images/badges/HTML.png",
+    badge3: "images/badges/CSS.png",
+    badge4: "",
+    description:
+      "A game with a scoring system that measures the highest score obtained as you are guessing the correct numbers.",
+  },
+];
