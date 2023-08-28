6c35ee7bcb31ce3f0f31d6d1f99e9885d31f279e
Jean Rosario <jean_carlosrosario709@outlook.com>
2023-08-28 15:58:12 -0700

    finished about section for mobile

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,23 @@ function About() {
 		<div id="about">
 			<div className={AboutCSS.mainContainer}>
 				<div className={AboutCSS.topTextContainer}>
-					<h3>I am a creative </h3>
+					<h3 className={AboutCSS.creativePerson}>I am a creative </h3>
+					<h3 className={AboutCSS.problemSolver}>PROBLEM SOLVER</h3>
+					<h3 className={AboutCSS.problemSolverbkg}>PROBLEM SOLVER</h3>
+					<h3 className={AboutCSS.passion}>with a passion for programming.</h3>
+				</div>
+				<div>
+					<img
+						className={AboutCSS.pcIMG}
+						src="https://ik.imagekit.io/o6xkxph96/JeanRosario/tr:w-300/my-portfolio-img/pc-img.png"
+						width={230}
+					/>
+				</div>
+				<div className={AboutCSS.lowerTextContainer}>
+					<h3 className={AboutCSS.lowerText}>
+						My expertise in various programming languages and technologies
+						allows me to design and develop efficient software solutions
+					</h3>
 				</div>
 			</div>
 		</div>
